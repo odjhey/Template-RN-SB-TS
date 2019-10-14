713e18f6192c20d9fd30ef7482b72a0dcccd2bc2
Odee <odjhey@github.com>
2019-10-14 11:31:36 +0800

    add rnstl

---
+++ components/storybook/storyLoader.js
@@ -0,0 +1,17 @@
+// Auto-generated file created by react-native-storybook-loader
+// Do not edit.
+//
+// https://github.com/elderfo/react-native-storybook-loader.git
+
+function loadStories() {
+	require('./stories/index.story');
+}
+
+const stories = [
+	'./stories/index.story'
+];
+
+module.exports = {
+  loadStories,
+  stories,
+};
